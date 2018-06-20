b6c1d9ea4b158a5854aefba93e074e2dbe139ff3
tacchan33 <39903348+tacchan33@users.noreply.github.com>
2018-06-20 16:02:42 +0900

    first commit

--- app/src/main/java/jp/ac/oit/is/lab261/sotsuken/BootReceiver.java
+++ app/src/main/java/jp/ac/oit/is/lab261/sotsuken/BootReceiver.java
@@ -1,7 +1,6 @@
 package jp.ac.oit.is.lab261.sotsuken;
 
 import android.content.BroadcastReceiver;
-import android.util.Log;
 import android.content.Context;
 import android.content.Intent;
 
@@ -11,8 +10,6 @@ public class BootReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent){
         if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             context.startService(new Intent(context,ControlService.class));
-            /* ログ出力 */
-            Log.d("debug","BootReceiver#onReceive()");
         }
     }
 
